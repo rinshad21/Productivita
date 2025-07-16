5c454c7914deacd4eb624456e43bd9152bb97ec4
Rinshad KA <mdrinshadka@gmail.com>
2025-07-16 22:58:39 +0530

    Remove invalid tailwind plugin and fix build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Crucial! Adjust if your components are in different paths
+  ],
   plugins: [tailwindcss(), react()],
   base: '/', 
 })
