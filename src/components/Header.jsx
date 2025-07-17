934f3f80aa3527d6715520b887b0b0f97a1f359e
Rinshad KA <mdrinshadka@gmail.com>
2025-07-17 17:34:47 +0530

    css fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 function Header() {
   return (
       <div className='text-center mr-20 mb-4 lg:mb-6'>
-          <h1 className="text-4xl font-bold font-serif  text-gray-800 mt-5" >Productivita</h1>
+          <h1 className="text-3xl lg:ext-4xl font-bold font-serif  text-gray-800 mt-5" >Productivita</h1>
           <p className="text-gray-500 italic mt-2">your all-in-one productivity App</p>
           </div>
   )
